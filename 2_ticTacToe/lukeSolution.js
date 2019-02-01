a3b69f6cdb5385bfea054d611394b039c5ca82b9
LukeBrandon <lukebrandon68@gmail.com>
2019-02-01 15:29:58 -0600

    need to finish

--- 2_ticTacToe/lukeSolution.js
+++ 2_ticTacToe/lukeSolution.js
@@ -5,16 +5,19 @@ const file = fs.readFileSync('./input.txt');
 const data = file.toString();
 const lines = data.split('\n');
 
-//loops through all of the lines
+//loops through all of the lines and passes data split at " " to function
 for(const line of lines){
-    let size = line.charAt(0);
-    console.log(size);
-
-    checkWinner(size, data);
+    let data = line.split(" ");
+    checkWinner(data);
 }
 
-function checkWinner(size, data) {
+function checkWinner(data) {
+    let size = data[0];     //size of board from the split
+    let boardData = data[1];    //board data
     let winner = "";
+    let board = [size][size];
+    let rows = boardData.split(size);
 
-    console.log(winner);
+    console.log(rows);
+    console.log(board);
 }
