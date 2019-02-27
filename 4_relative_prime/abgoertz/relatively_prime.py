399b8e2fb0ca07fdc1a6a095e2fb413516f500ce
AdamGoertz <36753247+AdamGoertz@users.noreply.github.com>
2019-02-26 22:56:52 -0600

    Improved my golf score

--- 4_relative_prime/abgoertz/relatively_prime.py
+++ 4_relative_prime/abgoertz/relatively_prime.py
@@ -1,10 +1,8 @@
-import math
+from math import gcd
+from functools import reduce
 
 def find_common_factors(numbers):
-    common_factor = numbers[0]
-    for n in numbers[1:]:
-        common_factor = math.gcd(n, common_factor)
-
+    common_factor = reduce(gcd, numbers)
     return common_factor if common_factor != 1 else True
 
 with open("input.txt", "r") as f:
